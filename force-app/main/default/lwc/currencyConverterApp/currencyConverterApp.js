1a077989abb2cf8e6e6e22f6de92dcb927b8f7d6
Gerax Sotelo <gsotelo23@gmail.com>
2024-07-04 16:04:51 -0500

    add country code list

--- force-app/main/default/lwc/currencyConverterApp/currencyConverterApp.js
+++ force-app/main/default/lwc/currencyConverterApp/currencyConverterApp.js
@@ -1,3 +1,17 @@
 import { LightningElement } from 'lwc';
+import { countryCodeList } from 'c/countryCodeList';
 
-export default class CurrencyConverterApp extends LightningElement {}
+export default class CurrencyConverterApp extends LightningElement {
+    countryList = countryCodeList;
+    countryFrom = "USD";
+    countryTo = "AUD";
+
+    handleChange(event) {
+        const {name, value} = event.target;
+        console.log("name: ", this[name]);
+        console.log("value: ", value);
+        this[name] = value;
+        console.log("name 2: ", this[name]);
+        console.log("value 2: ", value);
+    }
+}
