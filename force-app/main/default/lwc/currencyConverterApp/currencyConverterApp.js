65c8efda3efbf1d6d7290804f93e0166b7c31229
Gerax Sotelo <gsotelo23@gmail.com>
2024-07-12 13:45:27 -0500

    Display final result on component

--- force-app/main/default/lwc/currencyConverterApp/currencyConverterApp.js
+++ force-app/main/default/lwc/currencyConverterApp/currencyConverterApp.js
@@ -7,13 +7,33 @@ export default class CurrencyConverterApp extends LightningElement {
     countryList = countryCodeList;
     countryFrom = "USD";
     countryTo = "AUD";
+    amount = '';
+    result;
+    error;
 
     handleChange(event) {
         const {name, value} = event.target;
-        console.log("name: ", this[name]);
-        console.log("value: ", value);
         this[name] = value;
-        console.log("name 2: ", this[name]);
-        console.log("value 2: ", value);
+        this.result = '';
+        this.error = '';
+    }
+
+    submitHandler(event) {
+        event.preventDefault();
+        this.convert();
+    }
+
+    async convert() {
+        const API_KEY = '0f0197a69685751944d10da8';
+        const API_URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/pair/${this.countryFrom}/${this.countryTo}/${this.amount}`;
+        try {
+            const data = await fetch(API_URL);
+            const jsonData = await data.json();
+            console.log('JSON Data: ', jsonData);
+            this.result = jsonData.conversion_result.toFixed(2);
+        } catch(error) {
+            console.log('Error: ', error);
+            this.error = error;
+        }
     }
 }
