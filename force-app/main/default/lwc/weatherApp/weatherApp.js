7b026732d20c34cb59c0c92842fb7aa2830e70fa
Gerax Sotelo <gsotelo23@gmail.com>
2024-07-19 16:24:00 -0500

    add searchHandler and submitHandler functions

--- force-app/main/default/lwc/weatherApp/weatherApp.js
+++ force-app/main/default/lwc/weatherApp/weatherApp.js
@@ -1,3 +1,19 @@
 import { LightningElement } from 'lwc';
 
-export default class WeatherApp extends LightningElement {}
+export default class WeatherApp extends LightningElement {
+    cityName = '';
+
+    searchHandler(event) {
+        this.cityName = event.target.value;
+    }
+
+    submitHandler(event) {
+        event.preventDefault();
+        this.fetchData();
+    }
+
+    fetchData() {
+        // call api
+        console.log('city name: ', this.cityName);
+    }
+}
